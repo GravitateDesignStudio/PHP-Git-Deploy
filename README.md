7069694fa7f40e49fa49799f11b1caa8d17c1223
geoff-grav <geoff.gedde@gravitatedesign.com>
2015-03-30 17:03:04 -0700

    Changed Notes

--- README.md
+++ README.md
@@ -9,13 +9,13 @@ Usage
 
   include('gitdeploy.php');
 
-  $deploy = new GRAV_GIT_DEPLOY();
-  $deploy->allow_browser_access = true; // Change this to false when running from GitHub.  Set to true only for Testing.
-  $deploy->secret = 'XXXXXXXXXXXXX';
-  $deploy->remote = 'origin';
-  $deploy->branch = 'master';
-  $deploy->debug = true; // Remove this line or change to false after Successful Pushes.  This can be set to a file to keep a log.
-  $deploy->dry_run = true; // Remove this line or change to false after Testing is complete.
-  $deploy->run();
+  $gitdeploy = new GRAV_GIT_DEPLOY();
+  $gitdeploy->allow_browser_access = true; // Change this to false when running from GitHub.  Set to true only for Testing.
+  $gitdeploy->secret = 'XXXXXXXXXXXXX'; // GitHub Secret Key or $_GET['secret'] if running from the browser.
+  $gitdeploy->remote = 'origin';
+  $gitdeploy->branch = 'master';
+  $gitdeploy->debug = true; // Remove this line or change to false after Successful Pushes.  This can be set to a file to keep a log.
+  $gitdeploy->dry_run = true; // Remove this line or change to false after Testing is complete.
+  $gitdeploy->run();
 
 ```
--- gitdeploy.php
+++ gitdeploy.php
@@ -176,13 +176,13 @@ class GRAV_GIT_DEPLOY
 }
 
 /* Usage
-$deploy = new GRAV_GIT_DEPLOY();
-$deploy->allow_browser_access = true; // Change this to false when running from GitHub.  Set to true only to test from the browser.
-$deploy->secret = 'XXXXXXXXXXXXX'; // GitHub Secret Key or $_GET['secret'] if running from the browser.
-$deploy->remote = 'origin';
-$deploy->branch = 'master';
-$deploy->debug = true; // Remove this line or change to false after Successful Pushes.  This can be set to a file to keep a log.
-$deploy->dry_run = true; // Remove this line or change to false after Testing is complete.
-$deploy->run();
+$gitdeploy = new GRAV_GIT_DEPLOY();
+$gitdeploy->allow_browser_access = true; // Change this to false when running from GitHub.  Set to true only to test from the browser.
+$gitdeploy->secret = 'XXXXXXXXXXXXX'; // GitHub Secret Key or $_GET['secret'] if running from the browser.
+$gitdeploy->remote = 'origin';
+$gitdeploy->branch = 'master';
+$gitdeploy->debug = true; // Remove this line or change to false after Successful Pushes.  This can be set to a file to keep a log.
+$gitdeploy->dry_run = true; // Remove this line or change to false after Testing is complete.
+$gitdeploy->run();
 */
 
